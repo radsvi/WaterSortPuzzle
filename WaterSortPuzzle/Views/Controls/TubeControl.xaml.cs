17ac307ab004438fd357009436f3b85aa3d55708
Radomir Svihel <189124653+radsvi@users.noreply.github.com>
2025-06-27 22:48:35 +0200

    Adding TubeControl

---
+++ WaterSortPuzzle/Views/Controls/TubeControl.xaml.cs
@@ -0,0 +1,47 @@
+
+
+namespace WaterSortPuzzle.Views.Controls;
+
+public partial class TubeControl : ContentView
+{
+    //public TubeControl()
+    //{
+    //	InitializeComponent();
+    //}
+    internal TubeControl(MainWindowVM mainPageVM, int tubeId, LiquidColor[] liquidColors)
+    {
+        InitializeComponent();
+        //(this.Content as FrameworkElement).BindingContext = this;
+        BindingContext = this;
+
+        MainPageVM = mainPageVM;
+        TubeId = tubeId;
+        LiquidColors = liquidColors;
+    }
+    private MainWindowVM MainPageVM { get; }
+    public int TubeId { get; }
+
+
+
+    internal LiquidColor[] LiquidColors
+    {
+        get { return (LiquidColor[])GetValue(LiquidColorsProperty); }
+        set { SetValue(LiquidColorsProperty, value); }
+    }
+
+    // Using a BindableProperty as the backing store for LiquidColors.  This enables animation, styling, binding, etc...
+    public static readonly BindableProperty LiquidColorsProperty =
+        BindableProperty.Create(nameof(LiquidColors), typeof(LiquidColor[]), typeof(TubeControl));
+
+
+
+    //public string LiquidColors
+    //{
+    //    get { return (string)GetValue(LiquidColorsProperty); }
+    //    set { SetValue(LiquidColorsProperty, value); }
+    //}
+
+    //// Using a BindableProperty as the backing store for LiquidColors.  This enables animation, styling, binding, etc...
+    //public static readonly BindableProperty LiquidColorsProperty =
+    //    BindableProperty.Create(nameof(LiquidColors), typeof(string), typeof(TubeControl));
+}
