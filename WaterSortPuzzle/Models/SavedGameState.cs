9f1f463a35868dd58894978af33d1c274239a8d7
Radomir Svihel <189124653+radsvi@users.noreply.github.com>
2025-07-10 15:40:38 +0200

    Tube buttons Async - improving functionality for StepBack action

---
+++ WaterSortPuzzle/Models/SavedGameState.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WaterSortPuzzle.Models
+{
+    public class SavedGameState
+    {
+        public SavedGameState(LiquidColor[,] gameState, int source, int target)
+        {
+            GameState = gameState;
+            Source = source;
+            Target = target;
+        }
+
+        public LiquidColor[,] GameState { get; private set; }
+        public int Source { get; private set; }
+        public int Target { get; private set; }
+    }
+}
