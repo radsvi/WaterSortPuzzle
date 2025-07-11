3669ea26ccfa7ab916088b2574ceb99989252726
Radomir Svihel <189124653+radsvi@users.noreply.github.com>
2025-07-11 13:07:26 +0200

    Adding extra fonts to use as icons

--- WaterSortPuzzle/MauiProgram.cs
+++ WaterSortPuzzle/MauiProgram.cs
@@ -17,8 +17,27 @@ namespace WaterSortPuzzle
                 })
                 .ConfigureFonts(fonts =>
                 {
-                    fonts.AddFont("OpenSans-Regular.ttf", "OpenSansRegular");
-                    fonts.AddFont("OpenSans-Semibold.ttf", "OpenSansSemibold");
+                    //fonts.AddFont("OpenSans-Regular.ttf", "OpenSansRegular");
+                    //fonts.AddFont("OpenSans-Semibold.ttf", "OpenSansSemibold");
+                    fonts.AddFont("fa-brands-400.ttf", "FontAwesomeBrands");
+                    fonts.AddFont("fa-regular-400.ttf", "FontAwesomeRegular");
+                    fonts.AddFont("fa-solid-900.ttf", "FontAwesomeSolid");
+
+#if WINDOWS
+                    // Workaround: Remap font alias to actual Windows font file URI
+                    Microsoft.Maui.Handlers.LabelHandler.Mapper.AppendToMapping("FontFix", (handler, view) =>
+                    {
+                        if (view is Label label)
+                        {
+                            if (label.FontFamily == "FontAwesomeSolid")
+                                label.FontFamily = "ms-appx:///Resources/Fonts/fa-solid-900.ttf#Font Awesome 6 Pro Solid";
+                            else if (label.FontFamily == "FontAwesomeRegular")
+                                label.FontFamily = "ms-appx:///Resources/Fonts/fa-regular-400.ttf#Font Awesome 6 Free";
+                            else if (label.FontFamily == "FontAwesomeBrands")
+                                label.FontFamily = "ms-appx:///Resources/Fonts/fa-brands-400.ttf#Font Awesome 6 Brands";
+                        }
+                    });
+#endif
                 });
 
             builder.Services.AddSingleton<MainPage>();
