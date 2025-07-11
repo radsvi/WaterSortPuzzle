9106cffb04ed38f67da501f40a3a89f0f6174e7a
Radomir Svihel <189124653+radsvi@users.noreply.github.com>
2025-07-11 18:47:18 +0200

    Adding icons to Flyout

--- WaterSortPuzzle/MauiProgram.cs
+++ WaterSortPuzzle/MauiProgram.cs
@@ -19,7 +19,7 @@ namespace WaterSortPuzzle
                 {
                     //fonts.AddFont("OpenSans-Regular.ttf", "OpenSansRegular");
                     //fonts.AddFont("OpenSans-Semibold.ttf", "OpenSansSemibold");
-                    fonts.AddFont("fa-brands-400.ttf", "FontAwesomeBrands");
+                    //fonts.AddFont("fa-brands-400.ttf", "FontAwesomeBrands");
                     fonts.AddFont("fa-regular-400.ttf", "FontAwesomeRegular");
                     fonts.AddFont("fa-solid-900.ttf", "FontAwesomeSolid");
 
