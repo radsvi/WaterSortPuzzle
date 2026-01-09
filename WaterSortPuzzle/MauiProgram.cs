38236a1bc33991228624cd534b13797551956497
Radomir Svihel <189124653+radsvi@users.noreply.github.com>
2026-01-09 20:40:02 +0100

    Removing popup options from MauiProgram.cs

--- WaterSortPuzzle/MauiProgram.cs
+++ WaterSortPuzzle/MauiProgram.cs
@@ -17,24 +17,7 @@ namespace WaterSortPuzzle
 #pragma warning disable CA1416
             builder
                 .UseMauiApp<App>()
-                .UseMauiCommunityToolkit(options =>
-                {
-                    options.SetPopupDefaults(new DefaultPopupSettings
-                    {
-                        Margin = 0,
-                        Padding = 0,
-                        BackgroundColor = Colors.Red
-                    });
-                    options.SetPopupOptionsDefaults(new DefaultPopupOptionsSettings
-                    {
-                        Shadow = null,
-                        Shape = new Microsoft.Maui.Controls.Shapes.RoundRectangle
-                        {
-                            CornerRadius = new CornerRadius(0, 0, 0, 0),
-                            StrokeThickness = 0
-                        }
-                    });
-                })
+                .UseMauiCommunityToolkit()
                 .ConfigureFonts(fonts =>
                 {
                     //fonts.AddFont("OpenSans-Regular.ttf", "OpenSansRegular");
