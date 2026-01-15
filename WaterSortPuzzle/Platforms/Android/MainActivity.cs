e4ae0882fc738b90d3913a9adb00ba9e84b17549
Radomir Svihel <189124653+radsvi@users.noreply.github.com>
2026-01-15 15:35:11 +0100

    Changing navigation bar color

--- WaterSortPuzzle/Platforms/Android/MainActivity.cs
+++ WaterSortPuzzle/Platforms/Android/MainActivity.cs
@@ -1,6 +1,8 @@
 ﻿using Android.App;
 using Android.Content.PM;
 using Android.OS;
+using Android.Views;
+using Microsoft.Maui.Platform;
 
 namespace WaterSortPuzzle
 {
@@ -12,6 +14,42 @@ namespace WaterSortPuzzle
             base.OnCreate(savedInstanceState);
 
             RequestedOrientation = ScreenOrientation.Portrait;
+
+            UpdateNavigationBar();
         }
+#pragma warning disable CA1422, CA1416
+        private void UpdateNavigationBar()
+        {
+            var newColor = Android.Graphics.Color.Black;
+
+            if (Build.VERSION.SdkInt >= BuildVersionCodes.Tiramisu) // API 33+ is Android 13
+            {
+                // For Android 14 / API 35+, you can use WindowInsetsController and system bar appearance
+                var insetsController = Window?.InsetsController;
+
+                if (insetsController != null)
+                {
+                    // Set navigation bar background
+                    Window?.SetNavigationBarColor(newColor); // still works, but obsolete
+
+                    insetsController.SetSystemBarsAppearance(
+                        (int)WindowInsetsControllerAppearance.LightNavigationBars,
+                        (int)WindowInsetsControllerAppearance.LightNavigationBars);
+                }
+            }
+            else if (Build.VERSION.SdkInt >= BuildVersionCodes.Lollipop) // API 21+
+            {
+                // Pre-35 standard call
+                Window?.SetNavigationBarColor(newColor);
+
+                if (Build.VERSION.SdkInt >= BuildVersionCodes.R) // API 30+
+                {
+                    Window?.InsetsController?.SetSystemBarsAppearance(
+                        (int)WindowInsetsControllerAppearance.LightNavigationBars,
+                        (int)WindowInsetsControllerAppearance.LightNavigationBars);
+                }
+            }
+        }
+#pragma warning restore CA1416
     }
 }
