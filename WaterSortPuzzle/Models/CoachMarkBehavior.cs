2595d8269a0c94b6867120a9ab7a6c8d7724484e
Radomir Svihel <189124653+radsvi@users.noreply.github.com>
2026-01-16 00:59:48 +0100

    Adding new HelpPopup - adjusting CoachMarks - 4 - adjusting the description bubble - adding offset

--- WaterSortPuzzle/Models/CoachMarkBehavior.cs
+++ WaterSortPuzzle/Models/CoachMarkBehavior.cs
@@ -93,7 +93,7 @@ namespace WaterSortPuzzle.Models
 
 
 
-        private static Rect GetAbsoluteBounds(VisualElement element)
+        private Rect GetAbsoluteBounds(VisualElement element)
         {
             double x = element.X;
             double y = element.Y;
@@ -106,7 +106,76 @@ namespace WaterSortPuzzle.Models
                 parent = parent.Parent as VisualElement;
             }
 
+            y += 36;
+
             return new Rect(x, y, element.Width, element.Height);
         }
+        //public Rect GetAbsoluteBounds(VisualElement element)
+        //{
+        //    double x = element.X;
+        //    double y = element.Y;
+
+        //    Element parent = element.Parent;
+
+        //    while (parent is VisualElement parentElement)
+        //    {
+        //        // Add the parent's coordinates
+        //        x += parentElement.X;
+        //        y += parentElement.Y;
+
+        //        // If the parent is a ScrollView, we must subtract the scroll offset
+        //        if (parentElement is ScrollView scrollView)
+        //        {
+        //            x -= scrollView.ScrollX;
+        //            y -= scrollView.ScrollY;
+        //        }
+
+        //        parent = parentElement.Parent;
+        //    }
+
+        //    // If you want the coordinates of the CONTENT (inside the padding):
+        //    // x += element.Padding.Left;
+        //    // y += element.Padding.Top;
+
+        //    return new Rect(x, y, element.Width, element.Height);
+        //}
+        //private static Rect GetAbsoluteBounds(VisualElement element)
+        //{
+        //    if (element == null)
+        //        return Rect.Zero;
+
+        //    double x = element.X;
+        //    double y = element.Y;
+
+        //    var parent = element.Parent as VisualElement;
+        //    while (parent != null)
+        //    {
+        //        // accumulate parent offsets
+        //        x += parent.X;
+        //        y += parent.Y;
+
+        //        // add padding if parent is Layout
+        //        if (parent is Layout layout)
+        //        {
+        //            x += layout.Padding.Left;
+        //            y += layout.Padding.Top;
+
+        //            // if StackLayout, add spacing for preceding siblings
+        //            if (layout is StackLayout stack)
+        //            {
+        //                int index = stack.Children.IndexOf(element);
+        //                if (stack.Orientation == StackOrientation.Vertical)
+        //                    y += index * stack.Spacing;
+        //                else
+        //                    x += index * stack.Spacing;
+        //            }
+        //        }
+
+        //        element = parent;
+        //        parent = element.Parent as VisualElement;
+        //    }
+
+        //    return new Rect(x, y, element.Width, element.Height);
+        //}
     }
 }
