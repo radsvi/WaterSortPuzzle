13dbb35b40bd925cbe594a70b838cd5789f6a372
Radomir Svihel <189124653+radsvi@users.noreply.github.com>
2026-01-22 17:34:00 +0100

    Adding CoachMarks again - 3

--- WaterSortPuzzle/Views/CoachMarkOverlay.xaml.cs
+++ WaterSortPuzzle/Views/CoachMarkOverlay.xaml.cs
@@ -18,23 +18,61 @@ public partial class CoachMarkOverlay : AbsoluteLayout, IDrawable
 
     public void Show(VisualElement target, string text)
     {
-        int hintWidth = 250;
         HintLabel.Text = text;
+        Dispatcher.Dispatch(() =>
+        {
 
-        // Get target position on screen
-        var bounds = target.GetBoundsOnScreen();
-        _targetRect = new RectF(
-            (float)bounds.X,
-            (float)bounds.Y,
-            (float)bounds.Width,
-            (float)bounds.Height);
+            // Get target position on screen
+            Rect targetBounds = target.GetBoundsOnScreen();
+            _targetRect = new RectF(
+                (float)targetBounds.X,
+                (float)targetBounds.Y,
+                (float)targetBounds.Width,
+                (float)targetBounds.Height);
 
-        // Position hint below target
-        AbsoluteLayout.SetLayoutBounds(
-            HintFrame,
-            new Rect(bounds.X, bounds.Y, hintWidth, AbsoluteLayout.AutoSize));
+            Rect hintBounds = CalculateNewHintPosition(targetBounds);
 
-        OverlayGraphics.Invalidate();
+            // Position hint below target
+            AbsoluteLayout.SetLayoutBounds(
+                HintFrame,
+                hintBounds);
+
+            OverlayGraphics.Invalidate();
+        });
+    }
+    /// <summary>
+    ///  Calculate new position for hint. Based on screen size and position of the target element
+    /// </summary>
+    /// <param name="targetBounds"></param>
+    /// <returns></returns>
+    /// <exception cref="NullReferenceException"></exception>
+    private Rect CalculateNewHintPosition(Rect targetBounds)
+    {
+        int halfPadding = 6;
+
+        //var page = App.Current!.Windows[0].Page;
+        //if (page == null) throw new NullReferenceException($"{nameof(page)} is null");
+        //var displayInfo = DeviceDisplay.Current.MainDisplayInfo;
+        //double screenWidth = displayInfo.Width / displayInfo.Density;
+        //double screenHeight = displayInfo.Height / displayInfo.Density;
+
+        Rect hintBounds = HintFrame.GetBoundsOnScreen();
+
+        double xPos = this.Width / 2 - hintBounds.Width / 2;
+        double yPos;
+        if (targetBounds.Y + targetBounds.Height + (halfPadding * 2) + hintBounds.Height <= this.Height)
+        {
+            yPos = targetBounds.Y + targetBounds.Height + halfPadding;
+        }
+        else
+        {
+            yPos = targetBounds.Y - hintBounds.Height - halfPadding;
+        }
+
+
+        Rect newBounds = new Rect(xPos, yPos, HintFrame.Width, HintFrame.Height);
+        // nemusim updatovat i _targetRect?
+        return newBounds;
     }
 
     public void Draw(ICanvas canvas, RectF dirtyRect)
